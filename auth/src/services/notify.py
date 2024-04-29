80768acadbc34c63bc0d32a237f2908a7f534d6c
Olga Leonteva <olga.leonteva@telekom.com>
2024-04-30 00:20:56 +0200

    add worker_user_info

--- auth/src/services/notify.py
+++ auth/src/services/notify.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod, ABC
+import json
 import logging
 
 import httpx
@@ -18,14 +19,14 @@ class NotifyClient(NotifyAbstract):
     @staticmethod
     async def send_notify(user_id):
         data = {
-            'user_ids': [user_id],
+            'user_ids': [str(user_id)],
             'template_name': settings.welcome_template_name,
             'send_immediately': True,
         }
         # TODO добавить повторную отправку в случае недоступности апи
         async with httpx.AsyncClient() as client:
             try:
-                response = client.post(f'{settings.notify_api_url}/send_notification/', json=data)
+                response = await client.post(f'{settings.notify_api_url}/send_notification/', json=data)
                 response.raise_for_status()
             except httpx.HTTPError as exc:
                 logging.error(f'Problems with sending notification to user {user_id}: {exc}')
